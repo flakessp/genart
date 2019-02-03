de3662895edeb83cf48fde9e0779c80ff6aaa2aa
flakessp <flakessp@gmail.com>
2019-02-03 17:20:10 +0300

    linear interpolation

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp }  = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [2048, 2048]
@@ -7,31 +8,36 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 25;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = x / (count-1);
-        const v = y/ (count-1);
+        const v = y / (count-1);
         points.push([u,v])
       }
     }
     return points;
   }
   const points = createGrid();
-  console.log(points);
+  const margin = 300;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0,0,width,height);
 
     points.forEach(([u, v]) => {
-      const x = u * width;
-      const y = v * height;
+      // линейная интерполяция
+      const x = lerp(margin, width - margin, u);
+      // const x = u * width;
+      
+      // линейная интерполяция
+      const y = lerp(margin, width - margin, v);
+      // const y = v * height;
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.arc(x, y, 5, 0, Math.PI * 2, false);
       context.strokeStyle = 'black';
-      context.lineWidth = 20;
+      context.lineWidth = 10;
       context.stroke();
     })
   };
