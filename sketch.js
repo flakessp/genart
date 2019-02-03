9773a472d19e23665a4fdfd6385faaeb1f3b7bcc
flakessp <flakessp@gmail.com>
2019-02-03 19:40:42 +0300

    rotation

--- sketch.js
+++ sketch.js
@@ -7,30 +7,36 @@ const settings = {
   dimensions: [2048, 2048]
 };
 
+
+random.setSeed(random.getRandomSeed());
+console.log(random.getSeed());
+
 const sketch = () => {
-  const palette = random.shuffle(random.pick(palettes).slice(0,2));
+  const palette = random.shuffle(random.pick(palettes).slice(0,3));
   console.log(palette);
 
   const createGrid = () => {
     const points = [];
-    const count = 60;
+    const count = 50;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count-1);
         const v = count <= 1 ? 0.5 : y / (count-1);
-        const radius = Math.abs(random.noise2D(u, v) * 0.01)
+        const radius = Math.abs(random.noise2D(u, v) * 0.1)
         // pallete : ["#30261c", "#403831"]
         points.push({
           color: random.pick(palette),
           radius,
-          position: [ u, v ]
+          position: [ u, v ],
+          rotation: random.noise2D(u, v)
         })
       }
     }
     return points;
   }
   // const points = createGrid()
-  random.setSeed(110);
+  
+
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 300;
 
@@ -42,7 +48,8 @@ const sketch = () => {
       const {
         position, 
         radius,
-        color
+        color,
+        rotation
       } = data;
 
       const [u, v] = position;
@@ -55,14 +62,26 @@ const sketch = () => {
       const y = lerp(margin, width - margin, v);
       // const y = v * height;
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-      // context.strokeStyle = 'black';
-      // context.lineWidth = 5;
-      // context.stroke();
-      context.fillStyle = color;
-      context.fill();
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // // context.strokeStyle = 'black';
+      // // context.lineWidth = 5;
+      // // context.stroke();
+      // context.fillStyle = color;
+      // context.fill();
       
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius*width}px "Optician Sans"`;
+      context.translate(x,y);
+      context.rotate(rotation);
+      // context.fillText('F', x, y);
+      context.fillText('=', 0, 0);
+      // context.fillText('👻', 0, 0);
+
+      context.restore();
+
+
     })
   };
 };
